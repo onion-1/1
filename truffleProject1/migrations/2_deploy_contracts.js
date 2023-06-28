ac3b2a0877e1fd461022caf4c46fd6e86a34e6fe
onion <1903699137@qq.com>
2023-06-28 19:05:27 +0800

    delete

--- truffleProject1/migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const Bank = artifacts.require("Bank");
-const Attack = artifacts.require("Attack");
-module.exports = function(deployer) {
-    deployer.deploy(Bank).then(function() {
-        return deployer.deploy(Attack, Bank.address);
-    });
-};
